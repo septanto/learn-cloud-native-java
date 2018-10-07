64b1ade7602371f356ed60607ba68464d97ee2ec
Siprianus Septanto Adi Nugroho <s.s.adi.nugroho@gmail.com>
2018-10-07 22:29:09 +0700

    add hazelcast

--- product/src/main/java/com/mycompany/petstore/product/dao/ProductRepository.java
+++ product/src/main/java/com/mycompany/petstore/product/dao/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.mycompany.petstore.product.dao;
 
 import com.mycompany.petstore.product.entity.Product;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
+    @Cacheable("productsByCategoryCache")
     List<Product> findByCatId(int catId);
 }
